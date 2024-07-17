067da7abfc1ae967341b592de1dc75793c025896
mohtar25 <mokhtar2000naj@gmail.com>
2024-07-17 16:39:34 +0200

    fixing linitnig ;

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
+// eslint-disable-next-line
 const express = require("express");
 const app = express();
 
 // get the port from env variable
+// eslint-disable-next-line
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static("dist"));
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line
   console.log(`server started on port ${PORT}`);
 });
